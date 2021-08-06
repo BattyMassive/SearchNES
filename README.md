c43a9f486c172a7b39c418ee3722d52eac6c4a2d
Dylan Meithof <dylpickle2806@gmail.com>
2021-08-06 14:26:38 -0700

    Updated README, add requirements file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SearchNES
+
+
+##Setup
+```
+pip install -r requirements.txt
+set FLASK_ENV=development
+set FLASK_APP=server
+flask run
+https://dbpedia.org/page/Nintendo_Entertainment_System
+```
--- game_db.py
+++ game_db.py
@@ -10,25 +10,43 @@ def system_games():
         ?game dbo:computingPlatform {system} .
         ?game dbp:title ?title .
         ?game dbo:releaseDate ?releaseDate .
+        
     }}
-    LIMIT 1
+    LIMIT 10000
 """
     return fetch_results(query)
-
+#?genre rdfs:label ?genre_label .
+#?publisher dbp:name ?publisher_name .
 
 def game_info(game_uri):
     query = f"""
-    SELECT ?title, ?releaseDate, ?abstract
+    SELECT ?title, ?releaseDate, ?abstract, ?genre, ?publisher
 WHERE {{
 <{game_uri}> dbp:title  ?title .
 <{game_uri}>  dbo:releaseDate ?releaseDate .
-<{game_uri}> dbo:abstract ?abstract
+<{game_uri}> dbo:abstract ?abstract .
+<{game_uri}> dbo:genre ?genre .
+<{game_uri}> dbo:publisher ?publisher .
+}}
+"""
+    #print(query)
+    # returns only the first item
+    items = fetch_results(query)
+    if len(items) == 0:
+        return None
+    else:
+        return items[0]
+
+def publisher_info(uri):
+    query = f"""
+    SELECT ?name
+WHERE {{
+<{uri}> dbp:name  ?name .
 }}
 """
     # returns only the first item
     return fetch_results(query)[0]
 
-    
 def game_designers(game_uri):
     query = f"""
     SELECT ?designer, ?name, ?abstract
